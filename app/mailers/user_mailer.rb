e26eff30fe4a9ad3a8661df39b774509bcd873ee
philippegatti <ph.gatti@gmail.com>
2019-11-04 18:32:22 +0100

    1st version

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,15 @@
 class UserMailer < ApplicationMailer
-	default from: 'no-reply@monsite.fr'
+	default from: 'no-reply@ebbypg.fr'
 	
 	def welcome_email(user)
 	    @user = user 
-	    @url  = 'http://monsite.fr/login' 
+	    @url  = 'http://ebbypg.fr/login' 
     	mail(to: @user.email, subject: 'Bienvenue chez nous !') 
 	end
 	def participation_email(attendance)
 	    @user = User.find(attendance.user_id)
 	    @event = Event.find(attendance.event_id)
-	    @url  = 'http://monsite.fr/login' 
+	    @url  = 'http://ebbypg.fr/login' 
     	mail(to: @user.email, subject: 'Vous êtes inscrit à #{@event.title}!') 
 	end
 end
