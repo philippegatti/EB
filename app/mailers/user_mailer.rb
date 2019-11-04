c8bcd22398e2959f6950d02177295a24fea0a0e8
philippegatti <ph.gatti@gmail.com>
2019-11-04 13:43:25 +0100

    with mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+	default from: 'no-reply@monsite.fr'
+	def welcome_email(user)
+	    @user = user 
+	    @url  = 'http://monsite.fr/login' 
+    	mail(to: @user.email, subject: 'Bienvenue chez nous !') 
+	end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,14 @@ class User < ApplicationRecord
 	has_many :attendances
 	has_many :events
 	has_many :events, through: :attendances
+	
 	validates :first_name, presence: true
 	validates :last_name, presence: true
+	
+	after_create :welcome_send
+
+	def welcome_send
+		UserMailer.welcome_email(self).deliver_now
+	end
+
 end
