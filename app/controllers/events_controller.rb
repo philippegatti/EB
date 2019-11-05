8246b348d3582db574860ac9382050a4e9121a6a
philippegatti <ph.gatti@gmail.com>
2019-11-05 20:53:12 +0100

    5nov

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,8 +10,12 @@ class EventsController < ApplicationController
 	def new
 	end
 	def create
-		@event=Event.create(title: params[:title],price:params[:price],description:params[:description],admin_id:current_user.id,location: params[:location],start_date:params[:start_date],duration:params[:duration])
-		redirect_to event_path(@event.id) 
+		@event=Event.new(title: params[:title],price:params[:price],description:params[:description],admin_id:current_user.id,location: params[:location],start_date:params[:start_date],duration:params[:duration])
+		if @event.save
+			redirect_to event_path(@event.id)
+		else
+			redirect_to new_event_path
+		end
 	end
 	def edit
 		@event=Event.update(title: params[:title],price:params[:price],description:params[:description],admin_id:current_user.id,location: params[:location],start_date:params[:start_date],duration:params[:duration])
