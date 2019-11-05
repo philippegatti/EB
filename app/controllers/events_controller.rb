e289e0f5aa2996ec174dc666ea919a1979974d76
philippegatti <ph.gatti@gmail.com>
2019-11-05 17:42:35 +0100

    avec events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,16 @@
 class EventsController < ApplicationController
+	include ApplicationHelper
+	before_action :authenticate_user, except: [:index, :show]
 	def index
 		@events = Event.all
+	end
+	def show
+		@event= Event.find(params[:id])
+	end
+	def new
+	end
+	def create
+		@event=Event.create(title: params[:title],price:params[:price],description:params[:description],admin_id:current_user.id,location: params[:location],start_date:params[:start_date],duration:params[:duration])
+		redirect_to event_path(@event.id) 
 	end
 end
