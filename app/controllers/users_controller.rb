54d7f46509b10e8e19b3c7cd0e8bfb5c8bee4c3a
philippegatti <ph.gatti@gmail.com>
2019-11-05 21:13:07 +0100

    5nov

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+	include UsersHelper
+before_action :authenticate_user
+before_action :right_user
+
+
   def show
   	@user=User.find(params[:id])
   	@events=Event.where(admin_id:@user.id)
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+	def right_user
+  	unless current_user == User.find(params[:id])
+	  redirect_to new_user_session_path
+	end
+  	end
+
 end
